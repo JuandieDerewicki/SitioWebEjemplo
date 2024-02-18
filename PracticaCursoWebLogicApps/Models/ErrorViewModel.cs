e9b279dca7f7ef3b70c51f54cf47db51d8c7918e
JuandieDerewicki <jdderewicki@gmail.com>
2024-02-18 17:03:03 -0300

    Proyecto integrado

---
+++ PracticaCursoWebLogicApps/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PracticaCursoWebLogicApps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
